f17c0d5d2ee5ff93188d7808d9b58fe9630cc576
HakanKavci <hackankavci0@gmail.com>
2023-02-03 17:59:40 +0300

    .

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-// getFullYear() => Get the year as a four digit number (yyyy)
-
-// getMonth() => Get the month as a number (0-11)
-
-// getDate() => Get the day as a number (1-31)
-
-// getHours() => Get the hour (0-23)
-
-// getMinutes() => Get the minute (0-59)
-
-// getSeconds() => Get the second (0-59)
-
-// getDay() => Get the weekday as a number (0-6)
 
 const currentTime = document.getElementById("current-time")
 const meridiem = document.getElementById("am-pm")
